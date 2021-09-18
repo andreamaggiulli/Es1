2d3dec93986972c97c4dc46ee0c31879f18d5227
andreamaggiulli <amaggiulli@studenti.apuliadigitalmaker.it>
2021-09-18 08:46:57 +0200

    Initial commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table libri
+(
+    titolo varchar(100) not null,
+    autore varchar(100) not null,
+    anno_pb varchar(4) not null,
+    link varchar(100) not null
+);
---
+++ src/test/java/Esercizi/Es1/Es1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Esercizi.Es1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
